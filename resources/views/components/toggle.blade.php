29e61de89509ca749fcfb87dba12e26c3d5b817a
Kyle Klaus <kklaus@indemnity83.com>
2024-04-02 06:36:13 -0700

    feat: improve dark mode support for toggles

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -11,15 +11,15 @@
     {!! $attributes->merge(["class" => "mb-2 flex items-center"]) !!}
 >
     <div
-        class="{{ $checked ? "bg-gray-600" : "bg-gray-200" }} relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
+        class="{{ $checked ? "bg-gray-800" : "bg-gray-200 dark:bg-gray-900" }} relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2"
     >
         <span
             aria-hidden="true"
-            class="{{ $checked ? "translate-x-5" : "translate-x-0" }} pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+            class="{{ $checked ? "translate-x-5" : "translate-x-0" }} pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out dark:bg-gray-200"
         ></span>
     </div>
     <span class="ml-3 text-sm" id="{{ $id }}">
-        <span class="font-medium text-gray-900">
+        <span class="font-medium text-gray-600 dark:text-gray-400">
             {{ $slot }}
         </span>
     </span>
