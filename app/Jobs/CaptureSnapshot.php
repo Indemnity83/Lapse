da39a7336175926fa23dc3c2f08c788c6711d885
Kyle Klaus <kklaus@indemnity83.com>
2024-04-02 11:01:40 -0700

    improve media storage

--- app/Jobs/CaptureSnapshot.php
+++ app/Jobs/CaptureSnapshot.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Arr;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileCannotBeAdded;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
@@ -33,15 +34,37 @@ class CaptureSnapshot implements ShouldQueue
         $this->camera
             ->addMediaFromUrl($this->camera->url)
             ->usingName($this->getFilename())
-            ->toMediaCollection(config('media.snapshots'))
-            ->setCustomProperty('lapse_id', $this->lapse->id)
-            ->save();
+            ->usingFileName($this->getFilename())
+            ->withCustomProperties(['lapse_id' => $this->lapse->id])
+            ->toMediaCollection(config('media.snapshots'));
     }
 
     protected function getFilename(): string
     {
         $formattedDate = now()->format('Y-m-d-His');
+        $ext = $this->parseExtension($this->camera->url);
 
-        return "camera-{$this->camera->id}-{$formattedDate}";
+        return "camera-{$this->camera->id}-{$formattedDate}.$ext";
+    }
+
+    protected function parseExtension(string $url): string
+    {
+        $ext = pathinfo($url, PATHINFO_EXTENSION);
+
+        if ($ext !== '') {
+            return $ext;
+        }
+
+        $extensions = [
+            'image/jpeg' => 'jpeg',
+            'image/gif' => 'gif',
+            'image/png' => 'png',
+        ];
+
+        $type = get_headers($this->camera->url, 1)['Content-Type'];
+
+        // TODO: returns jpeg if nothing else could be found, would likely
+        //       result in corrupt files, should handle this better.
+        return Arr::get($extensions, $type, 'jpeg');
     }
 }
