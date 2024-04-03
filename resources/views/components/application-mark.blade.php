87b1982cd1fe47c0107b1cc75f97c150c3970b74
Kyle Klaus <kklaus@indemnity83.com>
2024-04-03 08:10:35 -0700

    add a logo

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,15 +1,11 @@
-<svg
-    viewBox="0 0 48 48"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {{ $attributes }}
->
-    <path
-        d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z"
-        fill="#6875F5"
-    />
-    <path
-        d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z"
-        fill="#6875F5"
-    />
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="86 127 328 246">
+  <path d="M365.7 371H134.3c-25.57 0-46.3-20.73-46.3-46.3V216.3c0-25.57 20.73-46.3 46.3-46.3h231.4c25.57 0 46.3 20.73 46.3 46.3v108.4c0 25.57-20.73 46.3-46.3 46.3z" fill="#90e0ef" stroke="#000" stroke-width="4" stroke-miterlimit="10"/>
+    <path d="M352.16 194H148.48l29.65-56.24c2.55-4.84 8.54-8.76 13.37-8.76h116.99c4.84 0 10.86 3.92 13.46 8.76L352.16 194z" fill="#00b4d8"/>
+    <path d="M352.16 194H148.48l29.65-56.24c2.55-4.84 8.54-8.76 13.37-8.76h116.99c4.84 0 10.86 3.92 13.46 8.76L352.16 194z" fill="none" stroke="#000" stroke-width="4" stroke-miterlimit="10"/>
+    <path d="M295.25 156h-90.5c-3.04 0-5.5-2.46-5.5-5.5s2.46-5.5 5.5-5.5h90.5c3.04 0 5.5 2.46 5.5 5.5s-2.46 5.5-5.5 5.5z" fill="#e6e6e6" stroke="#000" stroke-width="4" stroke-miterlimit="10"/>
+    <path d="M412 216.3c0-25.57-20.73-46.3-46.3-46.3H134.3c-25.57 0-46.3 20.73-46.3 46.3v21.63h324V216.3z" fill="#0077b6" stroke="#000" stroke-width="4" stroke-miterlimit="10"/>
+    <circle cx="373.5" cy="206.5" r="14.5" fill="#caf0f8" stroke="#000" stroke-width="4" stroke-miterlimit="10"/>
+    <circle cx="250" cy="270.5" r="78.75" fill="#00b4d8" stroke="#000" stroke-width="4" stroke-miterlimit="10"/>
+    <circle cx="250" cy="270.5" r="59.29" fill="#fff" stroke="#000" stroke-width="4" stroke-miterlimit="10"/>
+    <circle cx="227.5" cy="270.5" r="20.5" fill="#caf0f8"/>
 </svg>
