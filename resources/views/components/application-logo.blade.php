25b163badc9f8486db216fef0c8be7ac83aa3348
Kyle Klaus <kklaus@indemnity83.com>
2024-04-03 15:17:41 -0700

    update logo colors

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -4,64 +4,80 @@
     xml:space="preserve"
     viewBox="86 127 328 246"
 >
+    {{-- Camera Body --}}
     <path
+        class="fill-gray-200 dark:fill-gray-500"
         d="M365.7 371H134.3c-25.57 0-46.3-20.73-46.3-46.3V216.3c0-25.57 20.73-46.3 46.3-46.3h231.4c25.57 0 46.3 20.73 46.3 46.3v108.4c0 25.57-20.73 46.3-46.3 46.3z"
-        fill="#90e0ef"
-        stroke="#000"
         stroke-width="4"
         stroke-miterlimit="10"
+        stroke="#000000"
     />
+
+    {{-- Camera Finder --}}
     <path
+        class="fill-gray-200 dark:fill-gray-500"
         d="M352.16 194H148.48l29.65-56.24c2.55-4.84 8.54-8.76 13.37-8.76h116.99c4.84 0 10.86 3.92 13.46 8.76L352.16 194z"
-        fill="#00b4d8"
-    />
-    <path
-        d="M352.16 194H148.48l29.65-56.24c2.55-4.84 8.54-8.76 13.37-8.76h116.99c4.84 0 10.86 3.92 13.46 8.76L352.16 194z"
-        fill="none"
-        stroke="#000"
         stroke-width="4"
         stroke-miterlimit="10"
+        stroke="#000000"
     />
+
+    {{-- Finder Opening --}}
     <path
+        class="stroke-4 fill-gray-400 stroke-black"
         d="M295.25 156h-90.5c-3.04 0-5.5-2.46-5.5-5.5s2.46-5.5 5.5-5.5h90.5c3.04 0 5.5 2.46 5.5 5.5s-2.46 5.5-5.5 5.5z"
-        fill="#e6e6e6"
-        stroke="#000"
         stroke-width="4"
         stroke-miterlimit="10"
+        stroke="#000000"
     />
+
+    {{-- Top Bar --}}
     <path
+        class="fill-sky-500 dark:fill-sky-600"
         d="M412 216.3c0-25.57-20.73-46.3-46.3-46.3H134.3c-25.57 0-46.3 20.73-46.3 46.3v21.63h324V216.3z"
-        fill="#0077b6"
-        stroke="#000"
         stroke-width="4"
         stroke-miterlimit="10"
+        stroke="#000000"
     />
+
+    {{-- Flash --}}
     <circle
+        class="fill-sky-100 dark:fill-sky-300"
         cx="373.5"
         cy="206.5"
         r="14.5"
-        fill="#caf0f8"
-        stroke="#000"
         stroke-width="4"
         stroke-miterlimit="10"
+        stroke="#000000"
     />
+
+    {{-- Lense Ring --}}
     <circle
+        class="fill-sky-300 dark:fill-sky-400"
         cx="250"
         cy="270.5"
         r="78.75"
-        fill="#00b4d8"
-        stroke="#000"
         stroke-width="4"
         stroke-miterlimit="10"
+        stroke="#000000"
     />
+
+    {{-- Lense Glass --}}
     <circle
+        class="fill-white dark:fill-gray-400"
         cx="250"
         cy="270.5"
         r="59.29"
-        fill="#fff"
-        stroke="#000"
         stroke-width="4"
         stroke-miterlimit="10"
+        stroke="#000000"
     />
-    <circle cx="227.5" cy="270.5" r="20.5" fill="#caf0f8" />
+
+    {{-- Lense Spot --}}
+    <circle
+        class="fill-sky-100 dark:fill-gray-300"
+        cx="227.5"
+        cy="270.5"
+        r="20.5"
+    />
 </svg>
